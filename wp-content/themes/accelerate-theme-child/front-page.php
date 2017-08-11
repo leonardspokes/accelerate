5051b7be70c02ede343cac84d8566ddf90d8bcf2
Leonard Spokes <akonega13@gmail.com>
2017-08-11 08:53:14 -0400

    initial commit

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -23,6 +23,29 @@ get_header(); ?>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 </section><!-- .home-page -->
+
+<section class="featured-work">
+	<div class="site-content">
+		<h5>Featured Work</h5>
+		
+		<ul class="homepage-featured-work">
+			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				 <?php while ( have_posts() ) : the_post(); 
+					 $image_1 = get_field("image_1");
+					 $size = "medium"; ?>
+					 
+					 <figure>
+					 	<?php echo wp_get_attachment_image($image_1, $size); ?>
+					 </figure>
+					 
+					<h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+				  <?php endwhile; ?> 
+				<?php wp_reset_query(); ?>
+				
+	</div>
+</section>
+		
+
 <section class="recent-posts clearfix">
 	<div class="site-content">
     	<div class="blog-post">
@@ -31,8 +54,9 @@ get_header(); ?>
 					 <?php while ( have_posts() ) : the_post(); ?>
 						<h2><?php the_title(); ?></h2>
 						<?php the_excerpt(); ?>
-					  <?php endwhile; ?> 
-					<?php wp_reset_query(); ?>
+						<a href="<?php the_permalink(); ?>" class="read-more-link">
+					  <?php endwhile; // end of loop. ?> 
+					<?php wp_reset_query(); // resets the altered query back to the original ?>
 		 </div>
 	</div>
 </section>
